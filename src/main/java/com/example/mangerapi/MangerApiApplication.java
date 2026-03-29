a099d001efbbf81410df3caf1c2a2f297486336c
hakimsa <samouh591@gmail.com>
2026-03-29 20:30:27 +0200

    feature/crmt3232-login- v3

--- src/main/java/com/example/mangerapi/MangerApiApplication.java
+++ src/main/java/com/example/mangerapi/MangerApiApplication.java
@@ -8,7 +8,7 @@ public class MangerApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MangerApiApplication.class, args);
-		System.out.println("login successful v2");
+		System.out.println("login successful v3");
 	}
 
 }
